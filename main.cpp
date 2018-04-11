a735d138e384094e473d114a5654d93818ce3105
Alexandr Krasheninnikov <a.krasheninnikov@corp.badoo.com>
2018-04-11 20:03:06 +0300

    some code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include "proto/uds.pb.h"
+#include "clickhouse/client.h"
 
-static const std::string base64_chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz"
-        "0123456789+/";
-
-
-static inline bool is_base64(unsigned char c) {
-    return (isalnum(c) || (c == '+') || (c == '/'));
-}
-
-
-static std::string Decode(const std::string& input, std::string& out) {
+static std::string Decode(const std::string &input, std::string &out) {
     static constexpr unsigned char kDecodingTable[] = {
             64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
             64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
             64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
             52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
-            64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+            64, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
             15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
             64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
             41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
@@ -58,78 +49,81 @@ static std::string Decode(const std::string& input, std::string& out) {
 }
 
 
+int main(int argc, char* argv[]) {
+        std::cout << "Start" << std::endl;
 
-std::string base64_decode(std::string const& encoded_string) {
-    int in_len = encoded_string.size();
-    int i = 0;
-    int j = 0;
-    int in_ = 0;
-    unsigned char char_array_4[4], char_array_3[3];
-    std::string ret;
+        clickhouse::Client client(
+                clickhouse::ClientOptions()
+                        .SetHost("clickhouseuds1.mlan")
+                        .SetUser("krash")
+                        .SetPassword("krashclick")
+                        .SetCompressionMethod(clickhouse::CompressionMethod::LZ4)
+                        .SetPingBeforeQuery(false)
+        );
 
-    while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-        char_array_4[i++] = encoded_string[in_]; in_++;
-        if (i ==4) {
-            for (i = 0; i <4; i++)
-                char_array_4[i] = base64_chars.find(char_array_4[i]);
-
-            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-            for (i = 0; (i < 3); i++)
-                ret += char_array_3[i];
-            i = 0;
-        }
-    }
-
-    if (i) {
-        for (j = i; j <4; j++)
-            char_array_4[j] = 0;
-
-        for (j = 0; j <4; j++)
-            char_array_4[j] = base64_chars.find(char_array_4[j]);
-
-        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-    }
-
-    return ret;
-}
-
-int main()
-{
-    std::cout << "Start" << std::endl;
-
-    for(int j = 0; j<50; j++)
-    {
-        const clock_t begin_time = clock();
-        for(int i = 0; i<1000000;i++)
+        for(int i =0; i<3;i++)
         {
+            const clock_t begin_time = clock();
+            clickhouse::Block block;
+            auto dt = std::make_shared<clickhouse::ColumnDate>();
+            auto ts = std::make_shared<clickhouse::ColumnDateTime>();
+            auto event_name = std::make_shared<clickhouse::ColumnString>();
+            auto batch_id = std::make_shared<clickhouse::ColumnString>();
+            auto position = std::make_shared<clickhouse::ColumnUInt32>();
+            auto cnt = std::make_shared<clickhouse::ColumnInt32>();
+
+            auto key_arr = std::make_shared<clickhouse::ColumnArray>(std::make_shared<clickhouse::ColumnString>());
+            auto value_arr = std::make_shared<clickhouse::ColumnArray>(std::make_shared<clickhouse::ColumnString>());
 
             std::string out;
-            Decode("CIGHtdYFEh5Ib3RwYW5lbFxWaWV3UHJvZmlsZVBob3RvRXZlbnQYASIRYXBwbGljYXRpb24uYnJhbmQiFGFwcGxpY2F0aW9uLnBsYXRmb3JtIg91c2VyLmNvdW50cnlfaWQiC3VzZXIuZ2VuZGVyKgEyKgEzKgI5MioBMQ==", out);
 
-            uds_event event;
+            const std::basic_string<char> &batch_id_value = std::string("local_uds_debug_gpb-2018-04-10_008596");
+            std::ifstream input( argv[1] );
 
-            event.ParseFromString(out);
+            for( std::string line; getline( input, line ); )
+            {
+                Decode(line, out);
 
-            for(int k = 0; k<event.keys_size();k++) {
-                unsigned long smth = event.keys(k).size() + event.values(k).size();
-                if(i % 1234567891 == 0 && i != 0) {
-                    std::cout << smth << std::endl;
+                uds_event event;
+                event.ParseFromString(out);
+
+                dt->Append((time_t)event.ts());
+                ts->Append((time_t)event.ts());
+                event_name->Append(event.event_name());
+
+                batch_id->Append(batch_id_value);
+                position->Append(0);
+                cnt->Append(event.count());
+
+                auto keys = std::make_shared<clickhouse::ColumnString>();
+                auto vals = std::make_shared<clickhouse::ColumnString>();
+                for(int k = 0; k<event.keys_size();k++)
+                {
+                    keys->Append(event.keys(k));
+                    vals->Append(event.values(k));
                 }
+                key_arr->AppendAsColumn(keys);
+                value_arr->AppendAsColumn(vals);
             }
+
+            std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << " READ" << std::endl;
+
+            block.AppendColumn("dt", dt);
+            block.AppendColumn("ts", ts);
+            block.AppendColumn("event_name", event_name);
+            block.AppendColumn("batch_id", batch_id);
+            block.AppendColumn("position", position);
+            block.AppendColumn("cnt", cnt);
+            block.AppendColumn("keys", key_arr);
+            block.AppendColumn("vals", value_arr);
+
+
+            std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << " START" << std::endl;
+            client.Insert("default.krash_uds", block);
+
+            std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << " FINISH" <<  std::endl;
         }
-        std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    }
 
-
-
-    std::cout << "HELLO" << std::endl;
 
 
     return 0;
